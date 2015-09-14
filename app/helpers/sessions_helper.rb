e63bc8fd2e389c2f22afbc3fc372f0d6c03171de
Vivienne Lovelock <viv.lovelock@gmail.com>
2015-09-14 10:23:28 +1000

    bug fixes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
       if (user_id = session[:user_id])
